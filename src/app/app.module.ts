a9d8e45e339044bfa99d99d4c69bb8cf16477dc1
CodeChrisB <c.buchberger.01@gmail.com>
2020-10-10 14:22:53 +0200

    got angular material to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatSidenavModule } from "@angular/material/sidenav";
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +12,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
