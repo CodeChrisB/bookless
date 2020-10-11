423f8ff0dee460e6ba028c023c151fd559ee8169
CodeChrisB <c.buchberger.01@gmail.com>
2020-10-11 13:04:36 +0200

    linked main view parts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,19 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from "@angular/material/sidenav";
+import { TopbarComponent } from './main/topbar/topbar.component';
+import { SidenavComponent } from './main/sidenav/sidenav.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { ViewComponent } from './main/view/view.component';
+import { MainComponent } from './main/main/main.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    SidenavComponent,
+    FooterComponent,
+    ViewComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
