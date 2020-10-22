fcbe5942de7c599ecf42a35b6df09266f604ed95
CodeChrisB <c.buchberger.01@gmail.com>
2020-10-22 23:30:29 +0200

    added switch feature

--- src/app/main/sidenav/sidenav.component.ts
+++ src/app/main/sidenav/sidenav.component.ts
@@ -7,31 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent{
 
-  title = 'AngularMaterialGettingStarted';
-
-  isMenuOpen = true;
-  contentMargin = 240;
+  //current stat of the tabs
   crmOpen = false;
   billOpen =false;
   productOpen =false;
 
+  //close others tabs when open a new one
+  closeOnClick =false
+
 
   public openProduct() : void {
-    this.productOpen=true;
-    this.billOpen=false;
-    this.crmOpen=false;
+    this.productOpen=!this.productOpen;
+
+    if(this.closeOnClick){
+      this.billOpen=!this.productOpen;
+      this.crmOpen=false;
+    }
   }
 
   public openBill() : void {
-    this.productOpen=false;
-    this.billOpen=true;
-    this.crmOpen=false
+    this.billOpen=!this.billOpen;
+    
+    if(this.closeOnClick){
+      this.productOpen=false;
+      this.crmOpen=false
+    }
   }
 
   public openCrm() : void {
-    this.productOpen=false;
-    this.billOpen=false;
-    this.crmOpen=true
+    this.crmOpen=!this.crmOpen;
+    
+    if(this.closeOnClick){
+      this.productOpen=false;
+      this.billOpen=!this.crmOpen;
+    }
   }
 
 }
