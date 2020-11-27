cc328b953e8aaba79b17523f6e9e09347bd93cdb
Robert Freiseisen <r.superfreisi@gmail.com>
2020-11-27 21:22:33 +0100

    Connect to DB with one table

--- Backend/bookless_server/src/server.ts
+++ Backend/bookless_server/src/server.ts
@@ -1,8 +1,34 @@
+import { DH_UNABLE_TO_CHECK_GENERATOR } from "constants";
+import e from "express";
 import express from "express";
+import { Client, DataType }  from "ts-postgres";
+import { postgresqlErrorCodes } from "ts-postgres/dist/src/errors";
+async function main() {
+    const client = new Client({"host":"database", "port": 5432, "user": "user", "password":"pass", "database":"booklessdb"});
+    await client.connect();
+
+    console.log('db works')
+    try{
+        client.query("delete from test")
+        client.query("insert into test values('ddDASf')");
+        const resultIterator = client.query("select * from test")
+ 
+        for await (const row of resultIterator) {
+            console.log(row); // output data in the console
+        }
+    } finally{
+        await client.end();
+    }
+}
+
+
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hf  dd d d');
+    res.send('Hello Bookless');
 });
 
-app.listen(3000, () => console.log(' S d u ddn  ning'));
+app.listen(3000, () => {
+    main()
+    console.log('Running ');
+});
