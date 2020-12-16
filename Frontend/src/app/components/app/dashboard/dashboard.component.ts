94445fcec888f4f4036ede2ae3363dfe97dbe9eb
CodeChrisB <c.buchberger01@gmail.com>
2020-12-16 22:50:26 +0100

    populated the dashboard

--- Frontend/src/app/components/app/dashboard/dashboard.component.ts
+++ Frontend/src/app/components/app/dashboard/dashboard.component.ts
@@ -7,10 +7,26 @@ styleUrls: ['./dashboard.component.css']
 })
 export class Dashboard implements OnInit {
 
-constructor() {
-}
+  public now: String = "."
+
+  private weekDays=['Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag','Sontag'];
+  private months=['Jänner','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember'];
+  constructor() {
+      setInterval(() => {
+        this.setTime()
+      }, 1000);
+  }
+
+  setTime(){
+    var date = new Date();
+    this.now = this.weekDays[date.getUTCDay()-1]
+    this.now += date.toString().substring(7,11)
+    this.now+= this.months[date.getUTCMonth()]
+    this.now +=date.toString().substring(15,25)
+    console.dir(this.now)
+  }
 
 ngOnInit(){
-
+  this.setTime()
 }
 }
