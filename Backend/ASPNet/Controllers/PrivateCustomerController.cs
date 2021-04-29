607d4e47cc1501e277a31f5fa0c01903b2888397
CodeChrisB <c.buchberger.01@gmail.com>
2021-04-29 18:13:09 +0200

    AspDotNet Private customer Controller

--- Backend/ASPNet/Controllers/PrivateCustomerController.cs
+++ Backend/ASPNet/Controllers/PrivateCustomerController.cs
@@ -14,13 +14,37 @@ namespace ASPNet.Controllers
     {
       private static List<PrivateCustomer> customers = new List<PrivateCustomer>(){
           new PrivateCustomer(),
-          new PrivateCustomer{ Name="Egger"}
+          new PrivateCustomer{ Name="Egger" ,Id=1},
+          new PrivateCustomer{ Name="Egger" ,Id=2},
+          new PrivateCustomer{ Name="Egger" ,Id=3},
+          new PrivateCustomer{ Name="Egger" ,Id=4}
         };
 
-        [HttpGet]
+        [HttpGet("GetAll")]
         public ActionResult<List<PrivateCustomer>> Get()
         {
             return Ok(customers);
         }
+
+        [HttpGet("{id}")]
+        public ActionResult<List<PrivateCustomer>> GetSingle(int id)
+        {
+            return Ok(customers.FirstOrDefault(c=>c.Id==id));
+        }
+
+        [HttpGet("{begin}/{end}")]
+        public ActionResult<List<PrivateCustomer>> GetRange(int begin,int end)
+        {
+            return Ok(customers.GetRange(begin,end));
+        }
+
+        [HttpPost]
+        public ActionResult<List<PrivateCustomer>> AddCPrivateCustomer(PrivateCustomer customer){
+            customers.Add(customer);
+            return Ok(customer);
+        }
+        
+
+
     }
 }
