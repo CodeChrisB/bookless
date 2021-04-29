96f297bedbcb6f28decd049a94f6a81bd564ee0a
CodeChrisB <c.buchberger.01@gmail.com>
2021-04-29 16:54:29 +0200

    AspDotNet Customer Data Model

---
+++ Backend/ASPNet/Models/CRM/ContactPerson.cs
@@ -0,0 +1,14 @@
+namespace ASPNet.Models.CRM
+{
+    public class ContactPerson
+    {
+        public long id { get; set; }
+        public string adress { get; set; }
+        public string phoneNumber { get; set; }
+        public string companyRank { get; set; }
+        public string email { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public char gender { get; set; }
+    }
+}
--- Backend/ASPNet/Models/PrivateCustomer.cs
+++
@@ -1,8 +0,0 @@
-namespace ASPNet.Models
-{
-    public class PrivateCustomer
-    {
-        public int Id { get; set; }
-        public string Name {get; set;}
-    }
-}
