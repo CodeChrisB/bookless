9cef57970581b8fab94af9c10fbc01aa6d0de9ba
CodeChrisB <c.buchberger.01@gmail.com>
2021-05-17 08:26:39 +0200

    Private customer data

--- Backend/ASPNet/Bookless.Contracts/Persistence/CRM/IPrivateCustomer.cs
+++ Backend/ASPNet/Bookless.Contracts/Persistence/CRM/IPrivateCustomer.cs
@@ -6,10 +6,31 @@ namespace Bookless.Contracts.Persistence.CRM
 	[ContractInfo(ContextType = ContextType.Table)]
 	public partial interface IPrivateCustomer : IVersionable, ICopyable<IPrivateCustomer>
 	{
+		//private data
+		[ContractPropertyInfo(Required = true,  IsUnique = true)]
 		int CustomerId { get; set; }
-		[ContractPropertyInfo(Required = true, MaxLength = 1024, IsUnique = true)]
+		[ContractPropertyInfo(Required = true, MaxLength = 128)]
 		string fName { get; set; }
+		[ContractPropertyInfo(Required = true, MaxLength = 128)]
+		string lName { get; set; }
+		[ContractPropertyInfo(Required = true, MaxLength = 128)]
+		char gender { get; set; }
+		[ContractPropertyInfo(Required = true)]
+		string phone { get; set; }
 
-		//TODO add Private customer data
+		//adress data
+		[ContractPropertyInfo(Required = true, MaxLength = 128)]
+		string Town { get; set; }
+		[ContractPropertyInfo(Required = true, MaxLength = 128)]
+		string Plz { get; set; }
+		[ContractPropertyInfo(Required = true, MaxLength = 128)]
+		string Street { get; set; }
+		[ContractPropertyInfo(Required = true, MaxLength = 128)]
+		string Country { get; set; }
+
+		//order adress
+		[ContractPropertyInfo(Required = true)]
+		string[] orderAdress { get; set; }
+
 	}
 }
